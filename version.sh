f585551fa4b4b9490bd7156c3dc07c89c0cee626
Jero Eyras <jero.eyras@gmail.com>
2023-03-01 10:59:09 +0100

    New Version

--- version.sh
+++ version.sh
@@ -1,4 +1,23 @@
+set e-
+
+--msg=''
+
+
+
 git init
 git add -A
 git commit -m 'New Version'
-git push -f git@github.com:J3F31/npm-ms.git master
+git push -f git@github.com:J3F31/IWP.git master:gh-pages
+
+# PACKAGE_VERSION=bash npm pkg get version
+VERSION=$(node -p -e "require('./package.json').version")
+IFS='.' read -ra arr <<< $VERSION
+
+for i in "${arr[@]}"
+do
+    echo $i
+done
+
+major() {
+    echo 'test'
+}

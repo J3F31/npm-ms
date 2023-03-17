26aec6a1733f9867a0a25e2dc08c1a7874354114
Jero Eyras <jero.eyras@gmail.com>
2023-03-17 12:28:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@
 // import entityDefs from './config.json'
 // export * from './ecs-defs'
 
-export * from './Skybox/index'
+// export * from './Skybox/index'
 
-export * from './Scene/babylon-scene-component'
-export * from './Scene/create-scene-system'
+// export * from './Scene/babylon-scene-component'
+// export * from './Scene/create-scene-system'
 
-export * from './Mesh/babylon-mesh-component'
-export * from './Mesh/options-babylon-mesh'
-export * from './Mesh/create-mesh-system'
+// export * from './Mesh/babylon-mesh-component'
+// export * from './Mesh/options-babylon-mesh'
+// export * from './Mesh/create-mesh-system'
 
-export * from './Transforms/position-component'
+// export * from './Transforms/position-component'
 
-export * from './Camera/babylon-camera-component'
-export * from './Camera/create-camera-system'
+// export * from './Camera/babylon-camera-component'
+// export * from './Camera/create-camera-system'
 
 export * from './Test/CTest'
 export * from './Test/STest'

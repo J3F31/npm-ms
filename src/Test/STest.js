ca78a6592b6dd9893b55e44fee30e804008cd899
Jero Eyras <jero.eyras@gmail.com>
2023-03-17 12:19:40 +0100

    test

---
+++ src/Test/STest.js
@@ -0,0 +1,12 @@
+import { System } from '@lastolivegames/becsy'
+import { CTest } from './CTest'
+
+export class SystemWrite extends System {
+    test = this.singleton.write(CTest)
+
+    initialize() {
+        console.log(`Initial value of ${this.test.value}`)
+        this.test.value = 1
+        console.log(`Writing value to ${this.test.value}`)
+    }
+}
